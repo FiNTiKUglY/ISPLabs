7ef37f6ee313b59ef4fcaae0d22c41decec2c25c
FiNTiKUglY <74809370+FiNTiKUglY@users.noreply.github.com>
2021-04-21 02:44:38 +0300

    commit

--- lab6/lab6/IHuman.cs
+++ lab6/lab6/IHuman.cs
@@ -7,9 +7,9 @@ namespace lab6
 		public string Name { get; set; }
 		public string Surname { get; set; }
 		public int Age { get; set; }
-        public string Sex { get; set; }
-        public string Nationality { get; set; }
-        public void SmallInfo();
+		public string Sex { get; set; }
+		public string Nationality { get; set; }
+		public void SmallInfo();
 		public void GetInfo();
 	}
 }
