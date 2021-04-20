0d3835d7820e953ef5817b8f573c7a6507884abc
FiNTiKUglY <74809370+FiNTiKUglY@users.noreply.github.com>
2021-04-21 02:42:19 +0300

    commit

--- lab6/lab6/AthleteComparer.cs
+++ lab6/lab6/AthleteComparer.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 namespace lab6
 {
 	public class AthleteComparer: IComparer<Athlete>
 	{
 		public int Compare(Athlete first, Athlete second)
-        {
+		{
 			if (first.GetSpeciality()[0] > second.GetSpeciality()[0]) return 1;
 			else if (first.GetSpeciality()[0] == second.GetSpeciality()[0]) return 0;
 			else return -1;
-        }
+		}
 	}
 }
